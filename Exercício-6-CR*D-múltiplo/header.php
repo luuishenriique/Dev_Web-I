565a06d8af1e9c5b7b16d1bd2c22365e7409a437
Luis Henrique <luis_henrique_co@hotmail.com>
2019-09-26 15:42:17 -0300

    modificando e adicionando itens ao header.php

--- Exercício-6-CR*D-múltiplo/header.php
+++ Exercício-6-CR*D-múltiplo/header.php
@@ -6,6 +6,12 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<div class="nav">
-		<h3><a href="index.php">Pet's Land</a></h3>
+	<div class="nav_header">
+		<h3><a class="logo" href="index.php">Pet's Land</a></h3>
+		<div class="links">
+			<a href="index.php">Home</a>
+			<a href="pet.php">Pets</a>
+			<a href="owner.php">Donos</a>
+		</div>
 	</div>
+	<br>
