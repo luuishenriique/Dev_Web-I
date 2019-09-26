9e92f0ae0ac22372dd6e60be015c32a54d0c381e
Luis Henrique <luis_henrique_co@hotmail.com>
2019-09-26 15:47:15 -0300

    adicionando itens e corrigindo erros em pet.php

--- Exercício-6-CR*D-múltiplo/pet.php
+++ Exercício-6-CR*D-múltiplo/pet.php
@@ -16,6 +16,8 @@
 	</div>
 <?php endif ?>
 
+<br>
+
 <table class="pet_table">
 	<thead>
 		<tr>
@@ -30,12 +32,14 @@
 			<tr>
 				<td><?= $data['ID'] ?></td>
 				<td><?= $data['Nome do pet'] ?></td>
-				<td><a href="info.php?id=<?= $id ?>">Mais</a></td>
-				<td><a href="del_pet?id=<?= $id ?>">&times;</a></td>
+				<td><a href="info.php?id=<?= $id ?>">&#133;</a></td>
+				<td><a href="del_pet.php?id=<?= $id ?>">&times;</a></td>
 			</tr>
 		<?php endforeach ?>
 	</tbody>
 </table>
+
+<br>
 
 <form class="form_pet" action="add_pet.php" method="POST">
 	<fieldset class="main_form">
@@ -43,12 +47,14 @@
 		<input class="inp" type="text" name="id" placeholder="ID">
 		<input class="inp" type="text" name="petname" placeholder="Nome do pet">
 		<input class="inp" type="text" name="petdate" placeholder="Data de entrada">
+		<p style="text-align: center;">Selecione um dono para o pet:</p>
 		<select class="slt_own" name="select_owner">
 		<?php foreach ($data_own as $data): ?>
 			<option value="<?= $data ?>"><?= $data ?></option>
 		<?php endforeach ?>
 		</select>
 		<input class="btn" type="submit" value="Registrar">
+		<input class="btn" type="reset" value="Limpar">
 	</fieldset>
 </form>
 	
