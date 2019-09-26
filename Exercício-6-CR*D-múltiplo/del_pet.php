f4a76b44dd7996d7de796e4d4b257a650a614ea1
Luis Henrique <luis_henrique_co@hotmail.com>
2019-09-26 15:40:51 -0300

    corrigindo erro de sintaxe em linha

--- Exercício-6-CR*D-múltiplo/del_pet.php
+++ Exercício-6-CR*D-múltiplo/del_pet.php
@@ -2,11 +2,10 @@
 	require 'config.php';
 
 	$id = $_GET['id'];
-
 	$data = file(PET_FILE);
 	unset($data[$id]);
 
-	file_put_contents(PET_FILE, $data)
+	file_put_contents(PET_FILE, $data);
 
 	redirect("pet.php?msg=Registro apagado com sucesso! :D");
  ?>
